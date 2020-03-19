3f9ab78f4eecdd251d83b3ab02a204e938b71d81
Blend Ibraj <blendi.ibraj96@gmail.com>
2020-03-19 16:06:13 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -39,7 +39,7 @@ def full?(board)
 end
 
 def draw?(board)
-  if won?(board) == true
+  if won?(board) == false && full?(board) == true
     return false
   elsif won?(board) == false && full?(board) == false
   else
