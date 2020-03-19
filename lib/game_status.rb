f0a7a68d9356d873d5cfc2c03adc306079738472
Blend Ibraj <blendi.ibraj96@gmail.com>
2020-03-19 16:24:53 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -54,5 +54,15 @@ def over?(board)
   end
 end
 
-def winner(board)
+def winner (board)
+  index = []
+  index = won?(board)
+  if index == false
+    return nil
+  elsif board[index[0]] == "X"
+      return "X"
+    else
+      return "O"
+    end
+  end
 end
